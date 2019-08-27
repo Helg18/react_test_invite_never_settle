92a005a8153e11ac3a671d90a79221f6dc1a2394
Henry Leon Gomez <helg18@gmail.com>
2019-08-27 15:57:22 -0400

    Adding Main Background UI Gradients

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img width="100vh" src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
