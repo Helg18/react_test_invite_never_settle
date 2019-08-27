ea967b12ae0a102652b0d7b9b85ad25fcb8db135
Henry Leon Gomez <helg18@gmail.com>
2019-08-27 16:50:27 -0400

    Footer ready

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,11 @@ import './Footer.css';
 class Footer extends Component{
     render() {
         return(
-            <footer>
-                Footer Works.!
+            <footer className="text-white border-bottom-0">
+                <span>This app was a test provided by <a href="https://invite.com/" target="_blank"
+                                                                        rel="noopener noreferrer">Invite.com</a></span><br/>
+                <span>Developed by <a href="mailto:helg18@gmail.com" target="_blank"
+                                          rel="noopener noreferrer">Henry Leon</a></span>
             </footer>
         );
     }
